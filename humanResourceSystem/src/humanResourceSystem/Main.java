8f8d06658ae5e5242fc1581b4fd44638773696b3
Raiyan <Raiyan-sharif@users.noreply.github.com>
2020-07-10 14:01:19 +0600

    final change

--- humanResourceSystem/src/humanResourceSystem/Main.java
+++ humanResourceSystem/src/humanResourceSystem/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main {
 	static ArrayList<Student> students = new ArrayList<Student>();
 	static int opt2 = 1;
+	static int a,b;
 
 	public static void main(String[] args) {
         Scanner console= new Scanner(System.in);
@@ -19,6 +20,7 @@ public class Main {
          
          while(true) {
         	 if(opt2 == 0) {
+        		 System.out.println("BYE-BYE");
         		 break;
         	 }
         	 System.out.println("Main Menu \n1-View Staff\n2-Add New Staff\n3-Calculate Salary\n4-Exit");
@@ -30,7 +32,47 @@ public class Main {
         		 break;
         		 
         	 case 2:
-        		 addStudent();
+        		Student s1 = new Student();
+        		System.out.println("enter the details of staff ");
+        		System.out.print("enter the staff id: ");
+        		s1.id = console.nextLine();
+        		System.out.print("\nenter staff name: ");         
+        		s1.name = console.nextLine();
+        		System.out.print("\nenter the staff address: ");
+        		s1.address = console.nextLine();
+        		System.out.print("\nenter the staff phone number: ");
+        		s1.phoneNumber = console.nextLine();
+        		System.out.print("\nenter the staff email: ");
+        		s1.email = console.nextLine();
+        		addStudent(s1);
+        		break;
+        	 case 3:
+        		 System.out.println("SALARY CALCULATION");
+        		 int opt3=1;
+        		 while(opt3!=3) {
+        			 System.out.println("Enter basic salary: ");
+        			 b = console.nextInt();
+        			 System.out.println("Enter allowance: ");
+        			 a = console.nextInt();
+        			 System.out.println("Total salary is: "+ totalSalary(b,a));
+        			 System.out.println("Press 1 to calculate salary\nPress 2 to go to Main Menu\nPress 0 to exit\n");
+        			 opt3 = console.nextInt();
+        			 if(opt3 == 1) {
+        				 
+        			 }
+        			 else if(opt3==2) {
+        				 break;
+        			 }
+        			 else{
+        				 opt2=0;
+        				 break;
+        			 }
+        		 }
+        		 
+        		 break;
+        		 
+        	 case 4:
+        		 opt2 = 0;
         		 break;
         	 default:
         		 break;
@@ -49,20 +91,9 @@ public class Main {
         }
 	}
 	
-	public static void addStudent() {
+	public static void addStudent(Student s) {
 		Scanner in= new Scanner(System.in);
-		Student s = new Student();
-		System.out.println("enter the details of staff ");
-		System.out.print("enter the staff id: ");
-		s.id = in.nextLine();
-		System.out.print("\nenter staff name: ");         
-		s.name = in.nextLine();
-	    System.out.print("\nenter the staff address: ");
-	    s.address = in.nextLine();
-	    System.out.print("\nenter the staff phone number: ");
-	    s.phoneNumber = in.nextLine();
-	    System.out.print("\nenter the staff email: ");
-	    s.email = in.nextLine();
+		
 	    System.out.print("\nAre you sure you want to save this record (Y/N)");
 	    String opt = in.nextLine();
 	    if(opt.equals("Y")){
@@ -71,8 +102,19 @@ public class Main {
 	    System.out.println(" Press 1 to add more staff\nPress 2 to go to Main Menu\nPress 0 to exit");
 	    opt2 = in.nextInt();
 	    if(opt2 == 1) {
-	    	System. out. flush();
-	    	addStudent();
+	    	Student s1 = new Student();
+    		System.out.println("enter the details of staff ");
+    		System.out.print("enter the staff id: ");
+    		s1.id = in.nextLine();
+    		System.out.print("\nenter staff name: ");         
+    		s1.name = in.nextLine();
+    		System.out.print("\nenter the staff address: ");
+    		s1.address = in.nextLine();
+    		System.out.print("\nenter the staff phone number: ");
+    		s1.phoneNumber = in.nextLine();
+    		System.out.print("\nenter the staff email: ");
+    		s1.email = in.nextLine();
+    		addStudent(s1);
 	    }
 	    else if(opt2==2) {
 	    	
@@ -82,6 +124,9 @@ public class Main {
 	    }
 	    
 		
+	}
+	public static int totalSalary(int basic,int allowance) {
+		return basic+allowance;
 	}
 	
 	
